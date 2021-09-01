f2e7eb8d5a048b0796a10b379a9a045afa52ab2d
changqing <qing.chang@eeoa.com>
2021-09-01 14:38:12 +0800

    feat: 提交配置

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Author: changqing
+ * @Date: 2021-09-01 14:30:41
+ * @LastEditTime: 2021-09-01 14:30:42
+ * @LastEditors: changqing
+ * @Usage:
+ */
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
--- packages/lerna2/index.js
+++ packages/lerna2/index.js
@@ -2,7 +2,7 @@
  * @Description:
  * @Author: changqing
  * @Date: 2021-08-31 19:30:49
- * @LastEditTime: 2021-09-01 14:16:04
+ * @LastEditTime: 2021-09-01 14:36:44
  * @LastEditors: changqing
  * @Usage:
  */
